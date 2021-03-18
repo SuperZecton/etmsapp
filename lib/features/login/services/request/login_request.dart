6167d194751b2abd0c3104d773940cfe0fea6308
Darren Teh <darrentehh@gmail.com>
2021-03-19 01:45:37 +0800

    quick n dirty login auth
    inspired by the legend & myth PTE Aaron, they call him thelegend27

---
+++ lib/features/login/services/request/login_request.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+//class LoginRequest {
+  //LoginCtr con = new LoginCtr();
+
+  //Future<User> getLogin(String username, String password) {
+    //var result = con.getLogin(username, password);
+    //return result;
+ // }
+
+  //Future<void> createUser() async {
+    //con.saveUser(new User('_username', '_password'));
+ // }
+//}
