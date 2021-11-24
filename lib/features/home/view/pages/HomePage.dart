2616fa139a4bde6c45e3437b2a7c0210aaa1c57f
SuperZecton <Jovantan630@gmail.com>
2021-11-24 21:01:11 +0800

    New db

--- lib/features/home/view/pages/HomePage.dart
+++ lib/features/home/view/pages/HomePage.dart
@@ -126,7 +126,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                       InkWell(
                         onTap: (){
-                          Navigator.pushNamed(context, '/', );
+                          Navigator.pushNamed(context, '/mileageMain', );
                         },
                         child: Text(
                           "Show History",
---
+++ lib/newdbutils/database_link.dart
@@ -0,0 +1,6 @@
+import 'dart:async';
+
+class DatabaseCore {
+  //initialise DB link here
+
+}
