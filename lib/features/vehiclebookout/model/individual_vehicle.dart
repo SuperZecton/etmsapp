3b5e53e3b80cb5dabc05a062ee218586262739f4
Requlish <dlze2001@gmail.com>
2022-02-06 14:59:20 +0800

    Done Jovan's Homework

--- lib/features/vehiclebookout/model/individual_vehicle.dart
+++ lib/features/vehiclebookout/model/individual_vehicle.dart
@@ -1,17 +1,23 @@
 import 'dart:async';
 import 'package:path/path.dart';
 
-class VehType
+class CarType
 {
   final value;
-  const VehType._internal(this.value);
+  const CarType._internal(this.value);
   toString() => '$value';
   // TODO; Fill in with correct values
-  static const AType = const VehType._internal("A Type");
-  static const BType = const VehType._internal("B Type");
+  static const GPCar = const CarType._internal("GP Car");
+  static const Grade3 = const CarType._internal("Grade 3");
+  static const Grade2 = const CarType._internal("Grade 2");
+  static const Grade1 = const CarType._internal("Grade 1");
+  static const MPV = const CarType._internal("MPV");
+  static const MUV = const CarType._internal("MUV");
+  static const CLR = const CarType._internal("CLR");
+  static const OUV = const CarType._internal("OUV");
 
-  static List<VehType> list = [AType, BType];
-  static List<VehType> getValues() => list;
+  static List<CarType> list = [GPCar, Grade3, Grade2, Grade1, MPV, MUV, CLR, OUV];
+  static List<CarType> getValues() => list;
 }
 
 class VehClassType
