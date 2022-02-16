81584107c99dafa4cf1f25af1611855e92b6b0b6
Requlish <dlze2001@gmail.com>
2022-02-16 18:10:56 +0800

    Easiest Commit XD
    jovan help put data thanks :D

--- lib/core/services/TelebotConnector.dart
+++ lib/core/services/TelebotConnector.dart
@@ -6,11 +6,14 @@ import 'package:teledart/model.dart';
 class TelebotConnector {
   TelebotConnector();
 
-  Future<void> sendMovement(String vehicleNo, String locationEnd, String purposeOfTrip, String toRank, String toFullName, String vcRankFullName) async {
+  Future<void> sendMovement(String vehicleNo, String locationEnd, String purposeOfTrip, String toRank, String toFullName, String vcRankFullName, String additionalPlate) async {
     var botToken = "5269781876:AAFh-XytRqwAroOE2lpDA4FyaWfQqkkTUzg";
     var username = (await Telegram(botToken).getMe()).username;
     var teledart = TeleDart(botToken, Event(username!));
     teledart.start();
+    if (additionalPlate != ""){
+      vehicleNo = vehicleNo + " / " + additionalPlate;
+    }
     if (vcRankFullName == ""){
       teledart.sendMessage('-1001585228215', "BOS & CT/JIT DONE\nMID" + vehicleNo + " moving to " + locationEnd + " for " + purposeOfTrip + "\nTO: " + toRank + " " + toFullName);
     }
